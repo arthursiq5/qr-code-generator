773eb46e1ed14992d28fa052fc0ab198252aa753
arthursiq5 <arthur.siq5@gmail.com>
2024-12-22 14:11:13 -0300

    Added QR Code generation

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+const getQRCode = (url, size) => `https://quickchart.io/qr?size=300&text=${url}&format=png`
+
 const generateQRCode = (e) => {
     e.preventDefault()
     const url = document.getElementById('url').value
-    alert(url)
+    const qrCodeEl = document.getElementById('qrCode')
+    qrCodeEl.src = getQRCode(url);
 }
 
 document.getElementById('generateQRCode').addEventListener('submit', generateQRCode, false)
