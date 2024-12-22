1f72347b81bf5fc8e59b369b4c2f2342ec4da1c5
arthursiq5 <arthur.siq5@gmail.com>
2024-12-22 18:37:26 -0300

    Updated request and style

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const getQRCode = (url, size=300) => `https://quickchart.io/qr?size=${size}&text=${url}&format=png`
+const getQRCode = (url='', size=300) => `https://quickchart.io/qr?size=${size}&text=${url}&format=png`
 
 const generateQRCode = (e) => {
     e.preventDefault()
     const url = document.getElementById('url').value
     const size = document.getElementById('size').value
+    if (!url || !size) {
+        document.getElementById('qrCode').classList.add('hidden')
+        document.getElementById('log').innerHTML = 'Preencha os dados corretamente e tente novamente'
+        return;
+    }
+    document.getElementById('qrCode').classList.remove('hidden')
+    document.getElementById('log').innerHTML = '&nbsp;'
     const qrCodeEl = document.getElementById('qrCode')
+
     qrCodeEl.src = getQRCode(url, size);
 }
 
