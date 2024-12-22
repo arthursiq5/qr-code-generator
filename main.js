70c960fc0f611ee1fbf559e95ab34a8bd38a7baa
arthursiq5 <arthur.siq5@gmail.com>
2024-12-22 14:13:38 -0300

    Added size selection

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const getQRCode = (url, size=300) => `https://quickchart.io/qr?size=${size}&text
 const generateQRCode = (e) => {
     e.preventDefault()
     const url = document.getElementById('url').value
+    const size = document.getElementById('size').value
     const qrCodeEl = document.getElementById('qrCode')
-    qrCodeEl.src = getQRCode(url);
+    qrCodeEl.src = getQRCode(url, size);
 }
 
 document.getElementById('generateQRCode').addEventListener('submit', generateQRCode, false)
